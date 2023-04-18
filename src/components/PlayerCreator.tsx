6de666e2cde51604ac0a9025d0355449efe82002
Shawn <sdempsey@udel.edu>
2023-04-18 16:45:44 -0400

    added forwards to player pool

---
+++ src/components/PlayerCreator.tsx
@@ -0,0 +1,67 @@
+import "../App.css";
+import { Player } from "../interfaces/player";
+
+export function PlayerCreator() {
+    const forwardPool: Player[] = [
+        {
+            name: "Lionel Messi",
+            position: "Forward",
+            rating: 91,
+            imageURL: "/playerimages/messi.jpg"
+        },
+        {
+            name: "Karim Benzema",
+            position: "Forward",
+            rating: 91,
+            imageURL: "/playerimages/benzema.png"
+        },
+        {
+            name: "Erling Haaland",
+            position: "Forward",
+            rating: 88,
+            imageURL: "/playerimages/haaland.jpg"
+        },
+        {
+            name: "Eden Hazard",
+            position: "Forward",
+            rating: 84,
+            imageURL: "/playerimages/hazard.png"
+        },
+        {
+            name: "Robert Lewandowski",
+            position: "Forward",
+            rating: 91,
+            imageURL: "/playerimages/lewandowski.png"
+        },
+        {
+            name: "Kylian Mbappe",
+            position: "Forward",
+            rating: 91,
+            imageURL: "/playerimages/mbappe.png"
+        },
+        {
+            name: "Neymar Jr.",
+            position: "Forward",
+            rating: 89,
+            imageURL: "/playerimages/neymar.png"
+        },
+        {
+            name: "Cristiano Ronaldo",
+            position: "Forward",
+            rating: 89,
+            imageURL: "/playerimages/ronaldo.png"
+        },
+        {
+            name: "Mohamed Salah",
+            position: "Forward",
+            rating: 90,
+            imageURL: "/playerimages/salah.png"
+        },
+        {
+            name: "Son Heung-min",
+            position: "Forward",
+            rating: 89,
+            imageURL: "/playerimages/son.png"
+        }
+    ];
+} //end of func
