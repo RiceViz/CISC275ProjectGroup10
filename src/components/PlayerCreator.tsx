8e6d3fce00a89163c77bc3f9e445700fcb68559d
Shawn <sdempsey@udel.edu>
2023-04-18 18:47:42 -0400

    added goalkeepers to player pool

--- src/components/PlayerCreator.tsx
+++ src/components/PlayerCreator.tsx
@@ -2,7 +2,7 @@ import "../App.css";
 import { Player } from "../interfaces/player";
 
 export function PlayerCreator() {
-    const forwardPool: Player[] = [
+    const playerPool: Player[] = [
         {
             name: "Lionel Messi",
             position: "Forward",
@@ -62,6 +62,36 @@ export function PlayerCreator() {
             position: "Forward",
             rating: 89,
             imageURL: "/playerimages/son.png"
+        },
+        {
+            name: "Alisson Becker",
+            position: "Goalkeeper",
+            rating: 89,
+            imageURL: "/playerimages/alisson.png"
+        },
+        {
+            name: "Thibaut Courtois",
+            position: "Goalkeeper",
+            rating: 90,
+            imageURL: "/playerimages/courtois.png"
+        },
+        {
+            name: "Ederson",
+            position: "Goalkeeper",
+            rating: 89,
+            imageURL: "/playerimages/ederson.png"
+        },
+        {
+            name: "Manuel Neuer",
+            position: "Goalkeeper",
+            rating: 90,
+            imageURL: "/playerimages/neuer.png"
+        },
+        {
+            name: "Jan Oblak",
+            position: "Goalkeeper",
+            rating: 89,
+            imageURL: "/playerimages/oblak.png"
         }
     ];
 } //end of func
