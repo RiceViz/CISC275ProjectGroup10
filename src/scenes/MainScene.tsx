8e2c10f342ea0a2df962a52ecdcc8621aaadf7b8
Trevor <tkoenig@udel.edu>
2023-04-20 10:38:09 -0400

    starting to compartmentalize scenes to ease swapping

---
+++ src/scenes/MainScene.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./App.css";
+import { UserDropDownButton } from "../components/UserDropDownButton";
+import soccer from "../assets/soccer-landing.jpeg";
+import PlayerCard from "../components/PlayerCard";
+import { BiUserCircle } from "react-icons/bi";
+import Header from "../components/Header";
+import ThemeToggle from "../components/ThemeToggle";
+import { StartingLineup } from "../components/StartingLineup";
+import { Team } from "../interfaces/team";
+import { Player } from "../interfaces/player";
+import { PlayerCreator } from "../components/PlayerCreator";
+
+function App(): JSX.Element {
+    const newteam: Team = {
+        coach: "dan",
+        players: [],
+        lineup: [],
+        wins: 0,
+        losses: 0
+    };
+
+    const allPlayers: Player[] = PlayerCreator();
+
+    return (
+        <div
+            className="App"
+            style={{
+                backgroundImage: `url(${soccer})`,
+                backgroundSize: "cover"
+            }}
+        >
+            <Header>
+                <div className="flex flex-col dark:text-white">
+                    <div className="flex justify-between p-">
+                        <ThemeToggle></ThemeToggle>
+                        <div className="">
+                            <UserDropDownButton
+                                logo={<BiUserCircle size={25} />}
+                                options={[
+                                    "League Manager",
+                                    "Team Manager",
+                                    "Coach"
+                                ]}
+                            ></UserDropDownButton>
+                        </div>
+                    </div>
+                    <h1 className="text-4xl text-center dark:text-white">
+                        Soccer Football Manager
+                    </h1>
+                </div>
+            </Header>
+
+            <hr></hr>
+            <footer className="bg-neutral-50 dark:bg-neutral-900 dark:text-white">
+                Created by Trevor, Tyran, Mbiet, Shawn, & Gage
+            </footer>
+            <div>
+                <UserDropDownButton
+                    options={["League Manager", "Team Manager", "Coach"]}
+                ></UserDropDownButton>
+            </div>
+            <StartingLineup lineup={allPlayers}></StartingLineup>
+        </div>
+    );
+}
+
+export default App;
