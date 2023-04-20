542c608aadf886dfcec30f25fd0de86ba4bab65c
Trevor <tkoenig@udel.edu>
2023-04-20 11:31:12 -0400

    swapped name to Lineup so it more accurate

---
+++ src/components/Lineup.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Player } from "../interfaces/player";
+import { PlayerBanner } from "./PlayerBanner";
+
+/**
+ * Iterates a list of Players and creates a PlayerBanner for each
+ * @param lineup Player[]
+ * @returns JSX.Element
+ */
+export function Lineup({ lineup }: { lineup: Player[] }): JSX.Element {
+    return (
+        <div
+            style={{
+                justifyContent: "center",
+                display: "flex",
+                padding: "10px"
+            }}
+        >
+            <div>
+                {lineup.map(
+                    (player): JSX.Element => (
+                        <PlayerBanner
+                            player={player}
+                            key={player.name}
+                        ></PlayerBanner>
+                    )
+                )}
+            </div>
+        </div>
+    );
+}
