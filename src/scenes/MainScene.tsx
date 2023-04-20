b5a223c646209a0612ea59e737fd20d6bb69b21f
Trevor <tkoenig@udel.edu>
2023-04-20 11:30:48 -0400

    added some css to Lineup

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "../App.css";
 import { UserDropDownButton } from "../components/UserDropDownButton";
-import { StartingLineup } from "../components/StartingLineup";
+import { Lineup } from "../components/Lineup";
 import { Player } from "../interfaces/player";
 import { PlayerCreator } from "../components/PlayerCreator";
+import { Container, Row, Col } from "react-bootstrap";
 
 export function MainScene(): JSX.Element {
     const allPlayers: Player[] = PlayerCreator();
     return (
         <div>
-            <div>
-                <UserDropDownButton
-                    options={["League Manager", "Team Manager", "Coach"]}
-                ></UserDropDownButton>
-            </div>
-            <StartingLineup lineup={allPlayers}></StartingLineup>
+            <Container>
+                <Row>
+                    <UserDropDownButton
+                        options={["League Manager", "Team Manager", "Coach"]}
+                    ></UserDropDownButton>
+                    <Col>
+                        <div className="BoxedList">
+                            All Players
+                            <Lineup lineup={allPlayers}></Lineup>
+                        </div>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
