2145aba4eeeea6fb1221ecba93c63a4636d6e29f
Tyran Rice Jr <tyranjr@udel.edu>
2023-05-01 22:29:55 -0400

    Fixed warnings

--- src/components/WinFormula.tsx
+++ src/components/WinFormula.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { Team } from "../interfaces/team";
 import { Player } from "../interfaces/player";
 
 /*
