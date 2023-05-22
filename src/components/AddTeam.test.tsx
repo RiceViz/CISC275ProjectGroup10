49b0b8a4c60c0534d4c989803a4e6ac6696dc319
Trevor <tkoenig@udel.edu>
2023-05-22 16:55:01 -0400

    fixed  failing tests

--- src/components/AddTeam.test.tsx
+++ src/components/AddTeam.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
     render,
-    fireEvent,
-    getByTestId,
-    getByRole
+    fireEvent
+    // getByTestId,
+    // getByRole
 } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { AddTeamButton } from "./AddTeam";
@@ -11,18 +11,16 @@ import { AddTeamButton } from "./AddTeam";
 // Create a mock function for the addTeam prop
 const addTeamMock = jest.fn();
 
-describe("AddTeamButton", () => {
+describe("AddUserButton", () => {
     it("renders without crashing", () => {
         render(<AddTeamButton addTeam={addTeamMock} />);
     });
 
     it("opens and closes the modal", () => {
-        const { getByText, queryByText } = render(
-            <AddTeamButton addTeam={addTeamMock} />
-        );
+        const { getByText } = render(<AddTeamButton addTeam={addTeamMock} />);
 
         // Click the "Add Team" button
-        fireEvent.click(getByText("Add Team"));
+        fireEvent.click(getByText("Add User"));
 
         // The modal should now be visible
         expect(getByText("Add")).toBeInTheDocument();
@@ -32,12 +30,12 @@ describe("AddTeamButton", () => {
     });
 
     it("calls the addTeam prop when the Add Team button in the modal is clicked", () => {
-        const { getByText, getByLabelText, getByRole } = render(
+        const { getByText, getByLabelText } = render(
             <AddTeamButton addTeam={addTeamMock} />
         );
 
         // Click the "Add Team" button
-        fireEvent.click(getByText("Add Team"));
+        fireEvent.click(getByText("Add User"));
 
         // Enter some details for the new team
         fireEvent.change(getByLabelText("Name:"), {
--- src/components/TeamDropDownButton.test.tsx
+++ src/components/TeamDropDownButton.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { TeamDropDownButton } from "../components/TeamDropDownButton";
 import { Player } from "../interfaces/player";
